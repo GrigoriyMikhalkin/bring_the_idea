388838175a40e8ceee9f0a8a319a016e54e68c3e
GrigoriyMikhalkin <mikgrigoriy@yandex.ru>
2016-06-01 22:08:51 +0300

    settings.py added

---
+++ bring_the_idea/private_settings.py
@@ -0,0 +1,14 @@
+SECRET_KEY = "1"
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "btidb",
+        'USER': "",
+        'PASSWORD': "",
+        'HOST': '',
+        'PORT': '',
+    }
+}
