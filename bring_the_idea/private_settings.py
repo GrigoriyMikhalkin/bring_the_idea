2c3ceacad91f54325d1b9cd2e67deeeb3b5a6495
GrigoriyMikhalkin <mikgrigoriy@yandex.ru>
2016-06-10 10:25:40 +0300

    modified database settings

--- bring_the_idea/private_settings.py
+++ bring_the_idea/private_settings.py
@@ -1,3 +1,5 @@
+import os
+
 SECRET_KEY = "1"
 
 ALLOWED_HOSTS = []
@@ -5,10 +7,32 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv("POSTGRES_DB"),
+        'USER': "postgres",
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
+
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': "btidb",
-        'USER': "",
-        'PASSWORD': "",
+        'USER': "grigoriy",
+        'PASSWORD': "Gregornet123",
         'HOST': '',
         'PORT': '',
     }
 }
+
+services:
+  db:
+    image: postgres:9.4
+    environment:
+      POSTGRES_PASSWORD: 'secret'
+      POSTGRES_DB: 'demo'
+    volumes:
+      - /var/lib/postgresql/data
+"""
